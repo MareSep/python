b138d32ca0d93a170e180fe934d913819365f9cc
MareSep <mares@hkhk.edu.ee>
2024-12-05 16:28:50 +0200

    Update 04.py

--- 04.py
+++ 04.py
@@ -2,12 +2,19 @@
 # Mare 05.12.2024
 
 #4.1 Aia pikkus
-
+"""
 a = int(input("Sisesta külg 1: "))
 b = int(input("Sisesta külg 2: "))
 
 ymbermoot = 2*(a+b)
-print(f"Aia kogupikkus on {ymbermoot} meetrit.")
+print(f"Aia kogupikkus on {ymbermoot} meetrit.")"""
 
 #4.4 Kingituste pakkimine
-
+try:
+    karbi_suurus = 5
+    kingitused = int(input("Kingituste arv: "))
+    kastid = kingitused//karbi_suurus
+    jaak = kingitused % karbi_suurus
+    print(f"Saad teha {kastid} täis kinkekasti. Üle jääb {jaak} kingitust.")
+except:
+    print("Jälle tekitad probleeme!")
