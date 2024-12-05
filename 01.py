664766391252381961aae0a09653ca6df08b70a3
MareSep <mares@hkhk.edu.ee>
2024-12-05 14:38:45 +0200

    kilpkonn

---
+++ 01.py
@@ -0,0 +1,18 @@
+# 01. ülesanne
+
+import turtle
+
+turtle.shape("turtle")
+turtle.speed(4)
+turtle.penup()
+turtle.goto(-500, 200)
+turtle.pendown()
+
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+
+turtle.done()
