aad8c1de9d51fa668d7eafa5a4f8307641861cf7
MareSep <mares@hkhk.edu.ee>
2024-12-05 16:15:47 +0200

    Create 04.py
    Ülesanne 4

---
+++ 04.py
@@ -0,0 +1,13 @@
+# 4. ülesanne
+# Mare 05.12.2024
+
+#4.1 Aia pikkus
+
+a = int(input("Sisesta külg 1: "))
+b = int(input("Sisesta külg 2: "))
+
+ymbermoot = 2*(a+b)
+print(f"Aia kogupikkus on {ymbermoot} meetrit.")
+
+#4.4 Kingituste pakkimine
+
