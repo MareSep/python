b65d82f785ba097cf32148da02f1afb1c9beb5d3
MareSep <mares@hkhk.edu.ee>
2025-02-12 15:01:43 +0200

    kodutöö2

--- pangakonto.py
+++ pangakonto.py
@@ -1,5 +1,6 @@
 # PANGAKONTO
-# Looge funktsioon, mis võimaldab lisada või eemaldada summasid pangakontolt. Funktsioon peaks algama algse saldoga ja võimaldama teha operatsioone:
+# Looge funktsioon, mis võimaldab lisada või eemaldada summasid pangakontolt. Funktsioon peaks algama algse saldoga ja võimaldama 
+# teha operatsioone:
 #  deposiit (raha lisamine)
 #  väljavõte (raha eemaldamine)
 # Tagastage peale igat toimingut konto jääk.
@@ -10,8 +11,22 @@
 # Näide:
 # Alustades algse saldoga 100, deposiidi toiminguga 50, peaks funktsioon konto_haldur tagastama uue saldo 150.
 # Väljavõte toiminguga 20 uus saldo oleks siis 130.
-algne_saldo = 100
-summa = 
-deposiit = algne_saldo + summa
-valjavote = algne_saldo - summa
+fail = open("kontojaak.txt")
+algne_saldo = int(fail.read().strip())
+
 def kontohaldur(algne_saldo, toiming, summa):
+    if toiming == "deposiit":
+        kontojaak = algne_saldo + summa
+        with open("kontojaak.txt", "w") as fail:
+            fail.write(str(kontojaak))
+    elif toiming == "valjavote":
+        kontojaak = algne_saldo - summa
+        with open("kontojaak.txt", "w") as fail:
+            fail.write(str(kontojaak))
+    return kontojaak
+   
+
+toiming = input("Vali toiming: deposiit/valjavote: ")
+summa = abs(int(input("Sisesta summa: ")))  #teisendan sisendi absoluutväärtuseks, muidu saab negatiivset arvu kasutades 
+#vastupidiseid tehinguid teha
+print(f"Kontojääk on {kontohaldur(algne_saldo, toiming, summa)} eurot")
